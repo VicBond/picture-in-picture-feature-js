71364dfae4b146a170aa0761f272bdd9da55571d
Victor Bondarenko <nirasimha@gmail.com>
2022-08-12 07:56:00 -0400

    select media stream async func /add try catch

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
+//promt to select a video, pass to video element and play it
+
+async function selectMediaStrem() {
+  try {
+
+  } catch (e) {
+    // catch the error
+  }
+}
